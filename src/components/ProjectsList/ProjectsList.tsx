bb31393a1e1fe8265a4b2e1eeb42eddfef883df8
Svetlana <se55petrova@gmail.com>
2025-05-26 22:08:55 +0500

    Фильтрация проектов по статусу

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import styles from './ProjectsList.module.scss';
+import { useState } from 'react';
 
 const getStatusClass = (status: string) => {
   switch (status) {
@@ -16,15 +17,41 @@ const getStatusClass = (status: string) => {
 };
 
 export const ProjectsList = () => {
+  const [statusFilter, setStatusFilter] = useState('Все');
   const userId = useSelector((state: RootState) => state.auth.user?.id);
   const projects = useSelector((state: RootState) => state.projects.projects ?? []);
 
-  const visibleProjects = projects.filter((project) =>
-    project.access.includes(userId || '')
-  );
+  const visibleProjects = projects
+    .filter((project) => project.access.includes(userId || ''))
+    .filter((project) => statusFilter === 'Все' || project.status === statusFilter);
+
+  const statusOptions = ['Все', 'Срочно', 'В работе', 'На проверке', 'Ожидание'];
 
   return (
     <div className={styles.wrapper}>
+      <div className={styles.filterWrapper}>
+        <label>
+          Фильтр по статусу:{' '}
+          <select
+            className={styles.statusFilter}
+            value={statusFilter}
+            onChange={(e) => {
+              setStatusFilter(e.target.value);
+            }}
+          >
+            {statusOptions.map((status) => {
+              return (
+                <option
+                  key={status}
+                  value={status}
+                >
+                  {status}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
       {visibleProjects.length === 0 ? (
         <p>Нет доступных проектов.</p>
       ) : (
